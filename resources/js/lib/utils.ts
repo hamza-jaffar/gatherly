7a78ad6b425d44c7a571807100fc8bb77437fe7c
hamza-jaffar <hamzajaffer231006@gmail.com>
2026-02-01 21:22:26 +0500

    Initial Commit

---
+++ resources/js/lib/utils.ts
@@ -0,0 +1,15 @@
+import type { InertiaLinkProps } from '@inertiajs/react';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+export function toUrl(url: NonNullable<InertiaLinkProps['href']>): string {
+    return typeof url === 'string' ? url : url.url;
+}
+
+export function mergeNames(firstName: string, lastName: string): string {
+    return `${firstName} ${lastName}`;
+}
---
+++ resources/js/types/auth.ts
@@ -0,0 +1,27 @@
+export type User = {
+    id: number;
+    first_name: string;
+    last_name: string;
+    username: string;
+    phone: string;
+    avatar?: string;
+    email: string;
+    email_verified_at: string | null;
+    two_factor_enabled?: boolean;
+    created_at: string;
+    updated_at: string;
+    [key: string]: unknown;
+};
+
+export type Auth = {
+    user: User;
+};
+
+export type TwoFactorSetupData = {
+    svg: string;
+    url: string;
+};
+
+export type TwoFactorSecretKey = {
+    secretKey: string;
+};
