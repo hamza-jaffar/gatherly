4b53bc7c92f5ebae0f48754761b4a80182bdc7c0
hamza-jaffar <hamzajaffer231006@gmail.com>
2026-02-03 01:06:08 +0500

    Move Show Space Route to below other space route

--- routes/space.php
+++ routes/space.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\Space\SpaceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/spaces/{space:slug}', [SpaceController::class, 'show'])->name('space.show');
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('spaces', SpaceController::class)->names('space')->except('show');
 });
+
+Route::get('/spaces/{space:slug}', [SpaceController::class, 'show'])->name('space.show');
