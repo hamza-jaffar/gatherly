ae863a3d5571439e9316b237a90de89875c6c4ef
hamza-jaffar <hamzajaffer231006@gmail.com>
2026-02-09 17:14:07 +0500

    Update Logo

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,37 +1,5 @@
 import type { SVGAttributes } from 'react';
 
-export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
-    return (
-        <svg
-            {...props}
-            viewBox="0 0 28.66 8.77"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <text
-                transform="translate(0 6.75)"
-                fill="currentColor"
-                style={{
-                    fontSize: '8.06px',
-                    fontFamily: 'Inter, sans-serif',
-                }}
-            >
-                <tspan className="tracking-normal">D</tspan>
-                <tspan x="5.4" y="0">
-                    eg
-                </tspan>
-                <tspan x="13.95" y="0" style={{ letterSpacing: '-0.01em' }}>
-                    v
-                </tspan>
-                <tspan x="17.75" y="0">
-                    o
-                </tspan>
-                <tspan x="22.17" y="0" className="tracking-normal">
-                    r
-                </tspan>
-                <tspan x="24.77" y="0">
-                    a
-                </tspan>
-            </text>
-        </svg>
-    );
+export default function AppLogoIcon() {
+    return <img src="/logo.svg" alt="Gatherly Logo" />;
 }
--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -2,15 +2,15 @@ import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
-        <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+        <div className='flex gap-1 items-center'>
+            <div className="flex aspect-square size-8">
+                <AppLogoIcon />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    TOGATHERLY
+                    GATHERLY DEGVORA
                 </span>
             </div>
-        </>
+        </div>
     );
 }
